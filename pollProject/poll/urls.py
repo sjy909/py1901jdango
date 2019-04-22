33ee567f6a12c0bd375b986ca4d2ca2fc585fb0a
sjy <929497487@qq.com>
2019-04-22 08:53:52 +0800

    简易投票系统 编辑 添加功能

--- pollProject/poll/urls.py
+++ pollProject/poll/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     url(r'^index/$', views.index),
     url(r'detail/(\d+)', views.detail),
     url(r'vote/(\d+)$', views.vote),
+    url(r'^update/$', views.update),
+    url(r'^updates/$', views.updates),
+    url(r'^add/$', views.add),
+    url(r'^adds/$', views.adds)
 ]
